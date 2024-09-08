af7cb84084ab7272c6cbf6b45ffefef90220ce3d
heal333 <rohanaich333@gmail.com>
2024-09-08 19:21:06 +0530

    added new api endpoints for crypto coin site

--- index.js
+++ index.js
@@ -13,12 +13,53 @@ app.listen(PORT, () => {
     console.log("listening on port: ", PORT);
 });
 
+app.get("/", (req, res) => {
+    res.status(200).json({ youGot: "rooted" });
+});
 app.get("/coins", async (req, res) => {
-    const response = await fetch(`${process.env.API}?limit=50`, {
+    const response = await fetch(`${process.env.API}/latest?limit=50`, {
         headers: {
             "X-CMC_PRO_API_KEY": process.env.KEY,
         },
     });
     const result = await response.json();
     res.status(200).json(result);
+});
+app.get("/getCoinData", async (req, res) => {
+    const response = await fetch(
+        `https://pro-api.coinmarketcap.com/v2/cryptocurrency/info?id=${req.query.id}`,
+        {
+            headers: {
+                "X-CMC_PRO_API_KEY": process.env.KEY,
+            },
+        }
+    );
+    const result = await response.json();
+    res.status(200).json(result);
+});
+
+app.get("/convert", async (req, res) => {
+    const response = await fetch(
+        `https://pro-api.coinmarketcap.com/v2/tools/price-conversion?amount=${req.query.amount}&id=${req.query.fromid}&convert_id=${req.query.toid}`,
+        {
+            headers: {
+                "X-CMC_PRO_API_KEY": process.env.KEY,
+            },
+        }
+    );
+    const result = await response.json();
+    res.status(200).json(result);
+});
+
+app.get("/test", async (req, res) => {
+    // const response = await fetch(
+    //     `https://pro-api.coinmarketcap.com/v2/tools/price-conversion?id=1`,
+    //     {
+    //         headers: {
+    //             "X-CMC_PRO_API_KEY": process.env.KEY,
+    //         },
+    //     }
+    // );
+    // const result = await response.json();
+    // res.status(200).json(result);
 });
