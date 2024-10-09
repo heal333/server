70a252acb3658d9b628dd9ff614f92b4ce5c3450
heal333 <rohanaich333@gmail.com>
2024-10-09 21:07:05 +0530

    restoring a deleted cryptoCoin api path

--- src/routes/cryptoCoins/allCoins.js
+++ src/routes/cryptoCoins/allCoins.js
@@ -19,4 +19,17 @@ router.get("/coins", async (req, res) => {
     res.status(200).json(result);
 });
 
+router.get("/getCoinData", async (req, res) => {
+    const response = await fetch(
+        `https://pro-api.coinmarketcap.com/v2/cryptocurrency/info?id=${req.query.id}`,
+        {
+            headers: {
+                "X-CMC_PRO_API_KEY": process.env.KEY,
+            },
+        }
+    );
+    const result = await response.json();
+    res.status(200).json(result);
+});
+
 module.exports = router;
